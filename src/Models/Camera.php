863472a5489e1951b08851cf01c6a267fa0c9a6b
Kåre Slettnes <kaare.slettnes@tffk.no>
2021-08-26 15:16:05 +0200

    Camera model moved.

---
+++ src/Models/Camera.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TromsFylkestrafikk\Camera;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Camera extends Model
+{
+    use HasFactory;
+
+    protected $table = 'ip_cameras';
+    protected $guarded = ['id'];
+    protected $hidden = ['ip', 'mac'];
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+}
