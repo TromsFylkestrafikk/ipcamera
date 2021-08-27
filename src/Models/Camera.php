6503b13ef598d8bf732bf2ea3222d35f522ddaa0
Kåre Slettnes <kaare@slettnes.org>
2021-08-27 10:49:20 +0200

    Fix Camera model namespace

--- src/Models/Camera.php
+++ src/Models/Camera.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace TromsFylkestrafikk\Models\Camera;
+namespace TromsFylkestrafikk\Camera\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
