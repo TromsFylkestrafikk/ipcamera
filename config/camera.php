f403ec6840857538d8397c49984f9766d7e8559e
Kåre Slettnes <kaare.slettnes@tffk.no>
2021-11-08 10:12:53 +0100

    Issue #9: Use intervention image api instead of spatie.
    Also, it's possible to process images using a simple PHP include file
    per camera.
    
    commit e13b6e3bc2fbf8f3b47eb84bd0d64bdc31db21e1
    Author: Kåre Slettnes <kaare.slettnes@tffk.no>
    Date:   Mon Nov 8 10:11:57 2021 +0100
    
        Issue #9: Readme updated and config comments further tuned.
    
    commit e28916e1b171cf4e99ed55bb4bbd683c20bd79d5
    Author: Kåre Slettnes <kaare.slettnes@tffk.no>
    Date:   Mon Nov 8 10:07:55 2021 +0100
    
        Issue #9: Improved comments in config/camera.php
    
    commit a09c5334ee652b6aa1adadb562a47f64b796396a
    Author: Kåre Slettnes <kaare.slettnes@tffk.no>
    Date:   Mon Nov 8 08:43:21 2021 +0100
    
        Issue #9: Use Intervention + per camera config

--- config/camera.php
+++ config/camera.php
@@ -10,23 +10,32 @@ return [
         'middleware' => ['api'],
     ],
 
+    /*------------------------------------------------------------------------
+     |
+     | Disk, storage and file system settings
+     |
+     *------------------------------------------------------------------------
+     |
+     | NOTE! It is highly recommended to place image files in separate folders
+     | per camera, identified by a unique camera attribute.  This will optimize
+     | and simplify reverse camera detection on new image arrivals.
+     |
+     | Many parameters support tokens that substitutes various camera
+     | parameters. These tokens are encapsulated with double brackets,
+     | e.g. [[name]]. The following tokens can be used:
+     |
+     | - id:  Camera ID (Laravel internal ID)
+     | - camera_id:  The camera's internal ID.
+     | - name: Camera's name
+     | - mac: Camera's MAC address
+     | - ip: Camera's IP address.
+     | - latitude
+     | - longitude
+     *------------------------------------------------------------------------
+     */
+
     /**
-     * File system disk where incoming, raw camera files appear.
-     *
-     * NOTE! It is highly recommended to place image files in separate folders
-     * per camera, identified by a unique camera attribute.  This will optimize
-     * and simplify reverse camera detection on new image arrivals.
-     *
-     * The folder can contain tokens that substitutes various camera
-     * parameters. These tokens are encapsulated with double brackets,
-     * e.g. [[name]]. The following tokens can be used:
-     * - id:  Camera ID (Laravel internal ID)
-     * - camera_id:  The camera's internal ID.
-     * - name: Camera's name
-     * - mac: Camera's MAC address
-     * - ip: Camera's IP address.
-     * - latitude
-     * - longitude
+     * Laravel disk used for incoming files.
      */
     'incoming_disk' => 'camera_listen',
 
@@ -51,7 +60,8 @@ return [
     /**
      * Regex pattern of filename within camera directory.
      *
-     * Macro-expandable.
+     * Supports tokens. Note that tokens are expanded first, and any regex
+     * characters within these are escaped.
      */
     'file_regex' => '[[camera_id]].*\.(?i:jpe?g)',
 
@@ -63,11 +73,18 @@ return [
      */
     'pick_first_match' => false,
 
+    /*------------------------------------------------------------------------
+     |
+     | Cache, optimization, tweaks
+     |
+     *------------------------------------------------------------------------
+     */
+
     /**
      * Attach image as base64-encoded data on broadcast events when image files
      * are below this many bytes. Set to 0 or false to disable.
      */
-    'base64_encode_below' => 64000,
+    'base64_encode_below' => 32000,
 
     /**
      * Lifetime of current camera image.
@@ -93,4 +110,45 @@ return [
      * seconds to look for the latest file for each camera.
      */
     'poor_mans_inotify' => false,
+
+    /*------------------------------------------------------------------------
+     |
+     | Per camera image processing
+     |
+     *------------------------------------------------------------------------
+     |
+     | Images may be modified using the Intervention image API with custom
+     | config per camera. The "configuration" is a PHP script that returns a
+     | closure which accepts an image object and camera model as arguments.  An
+     | example config for a camera may be:
+     |
+     |     <?php
+     |
+     |     use Intervention\Image\Image;
+     |     use TromsFylkestrafikk\Camera\Models\Camera;
+     |
+     |     return function (Image $image, Camera $camera) {
+     |         $image->->crop(720, 406, 10, 30);
+     |     };
+     |
+     | @see http://image.intervention.io/
+     |
+     *------------------------------------------------------------------------
+     */
+
+    /**
+     * Directory containing per-camera image processors.
+     *
+     * This is relative to app root, i.e. laravel root folder containing 'app',
+     * 'config', 'routes', 'storage', etc.
+     */
+    'processor_dir' => 'config/camera_processors',
+
+    /**
+     * File pattern on custom image processors.
+     *
+     * Supports model macros. See section 'Disk, storage and file system
+     * settings'
+     */
+    'processor_inc' => '[[id]]-[[name]].inc',
 ];
